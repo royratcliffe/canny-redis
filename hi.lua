20508d241fa35a022349113254ce5b4698d287fa
Roy Ratcliffe <roy@ratcliffe.me>
2024-04-24 11:19:59 +0100

    docs: docker redis/redis-stack et al

--- hi.lua
+++ hi.lua
@@ -28,6 +28,11 @@ local metat = {
   end)
 }
 
+--- Answers an assertion with finaliser function.
+-- Creates a function that acts like `assert` but with one additional useful
+-- feature: it invokes a finaliser function just before raising an error.
+-- @tparam ?func finaliser Optional finaliser function.
+-- @return Assertion with optional finaliser.
 function _M.newtry(finaliser)
   return function(...)
     if not select(1, ...) then
@@ -43,6 +48,12 @@ end
 -- never be necessary in practice. The connection automatically closes on error.
 --
 --    assert(redis.call.sock:close())
+--
+-- Create a temporary Redis server using Docker as follows. It pulls version
+-- 7.2.4 Redis (at the time of writing) and publishes the standard Redis 6379
+-- port on the local host and its RedisInsight web interface at port 8001.
+--
+--    docker run -p 6379:6379 -p 8001:8001 --rm redis/redis-stack
 --
 _M.redis = socket.protect(function(url)
   local parsedurl = assert(socket.url.parse(url
