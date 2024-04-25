54273a258b69fd495b4c739e4023b6135e956fa6
Roy Ratcliffe <roy@ratcliffe.me>
2024-04-25 09:19:29 +0100

    test(redis): scan

--- spec/redis_spec.lua
+++ spec/redis_spec.lua
@@ -5,4 +5,51 @@ describe("redis", function()
     local lolwut = redis.call { "LOLWUT", "VERSION", tostring(7) }
     assert.are.equal(lolwut, redis.call("LOLWUT", "VERSION", tostring(7)))
   end)
+
+  describe("scan", function()
+    local key = "key"
+
+    before_each(function()
+      redis.call("DEL", key)
+    end)
+
+    after_each(function()
+      redis.call("EXPIRE", key, tostring(30))
+    end)
+
+    it("hash", function()
+      redis.call("HSET", key, "field", "value")
+      local actual = {}
+      for field, value in redis.hscan(key) do
+        table.insert(actual, { field, value })
+      end
+      assert.are.same({ { "field", "value" } }, actual)
+    end)
+
+    it("hash without values", function()
+      redis.call("HSET", key, "field", "value")
+      assert.has_error(function()
+        redis.hscan("key", { NOVALUES = true })()
+      end, "ERR syntax error")
+    end)
+
+    it("unordered set", function()
+      redis.call("SADD", key, "c", "b", "a", "a")
+      local actual = {}
+      for member in redis.sscan(key, { MATCH = "*", COUNT = 1 }) do
+        table.insert(actual, member)
+      end
+      table.sort(actual)
+      assert.are.same({ "a", "b", "c" }, actual)
+    end)
+
+    it("ordered set", function()
+      redis.call("ZADD", key, tostring(100), "value")
+      local actual = {}
+      for member, score in redis.zscan(key) do
+        table.insert(actual, { member, score })
+      end
+      assert.are.same({ { "value", 100 } }, actual)
+    end)
+  end)
 end)
