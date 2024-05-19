4976d0b255af4b08861091f9b2af6acc920cd65c
Roy Ratcliffe <roy@ratcliffe.me>
2024-05-19 15:09:11 +0100

    feat: redis.argv

---
+++ redis/argv.lua
@@ -0,0 +1,38 @@
+local _M = {}
+
+local __ARGV__index = {}
+
+local __ARGV = { __index = __ARGV__index }
+
+function __ARGV__index:insert(key, value)
+  if type(key) == "string" then
+    if type(value) == "string" then
+      table.insert(self, key)
+      table.insert(self, value)
+    elseif type(value) == "number" then
+      table.insert(self, key)
+      table.insert(self, tostring(value))
+    elseif type(value) == "boolean" and value then
+      table.insert(self, key)
+    end
+  end
+end
+
+function __ARGV__index:select(...)
+  for index = 1, select("#", ...) do
+    local arg = select(index, ...)
+    if type(arg) == "table" then
+      for key, value in pairs(arg) do
+        self:insert(key, value)
+      end
+    else
+      self:insert(arg, true)
+    end
+  end
+end
+
+function _M.new(...)
+  return setmetatable({}, __ARGV)
+end
+
+return _M
