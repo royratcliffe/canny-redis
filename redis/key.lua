b8b1ebe90b32a18a5e4ac92b25963cd5ab5129ef
Roy Ratcliffe <roy@ratcliffe.me>
2024-05-19 10:14:32 +0100

    fix(redis.key): __indexed and __index

--- redis/key.lua
+++ redis/key.lua
@@ -10,13 +10,13 @@ local __KEY = {}
 -- The module-level meta-index constructs a new key, or a new array using the
 -- given index and a meta-table of `__KEY`.
 function __M:__index(index)
-  return setmetatable({ index }, __KEY)
+  return setmetatable({ __index = index }, __KEY)
 end
 
 -- Indexing a key creates a new sub-key. The new key references the original.
 -- Mutating the original (nothing prevents it) affects all its descendants.
 function __KEY:__index(index)
-  return setmetatable({ self, index }, __KEY)
+  return setmetatable({ __indexed = self, __index = index }, __KEY)
 end
 
 -- Operates recursively.
@@ -27,8 +27,8 @@ end
 --    return self[2] and self[1] .. ":" .. self[2] or self[1]
 --
 function __KEY:__tostring()
-  local first, second = unpack(self)
-  return second and tostring(first) .. ":" .. second or first
+  local indexed, index = rawget(self, "__indexed"), rawget(self, "__index")
+  return indexed and tostring(indexed) .. ":" .. index or tostring(index)
 end
 
 function __KEY:__concat(other)
--- spec/redis/key_spec.lua
+++ spec/redis/key_spec.lua
@@ -1,11 +1,22 @@
 describe("redis.key", function()
-    local REDIS_KEY = require "redis.key"
+  local REDIS_KEY = require "redis.key"
 
-    it("works", function()
-        local key = REDIS_KEY.hello["world"]
-        assert.are.same({ { "hello" }, "world" }, key)
-        assert.are.equal("hello:world", tostring(key))
-        assert.are.equal("prefix_hello:world", "prefix_" .. key)
-        assert.are.equal("hello:world_suffix", key .. "_suffix")
-    end)
+  it("hello[1].worlds", function()
+    local key = REDIS_KEY.hello[1].worlds
+    assert.are.same({
+      __indexed = {
+        __indexed = {
+          __index = "hello" },
+        __index = 1
+      },
+      __index = "worlds"
+    }, key)
+    assert.are.equal("hello:1:worlds", tostring(key))
+    assert.are.equal("prefix_hello:1:worlds", "prefix_" .. key)
+    assert.are.equal("hello:1:worlds_suffix", key .. "_suffix")
+  end)
+
+  it("REDIS_KEY[1]", function()
+    assert.are.equal("1", tostring(REDIS_KEY[1]))
+  end)
 end)
