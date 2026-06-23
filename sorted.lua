1cba8a565f124a150aae1c4466f9d5fd3383bbff
Roy Ratcliffe <roy@ratcliffe.me>
2026-06-23 07:40:37 +0100

    docs(sorted.lua): enhance documentation for ordered function
    Add detailed parameter descriptions for the ordered function
    and clarify the behaviour of the ipairs function in sorted.lua.

--- sorted.lua
+++ sorted.lua
@@ -5,6 +5,9 @@
 -- @license MIT
 local _M = {}
 
+-- Iterates an ordering of values by their sorted indices.
+-- @tparam tab sorted Sorted indices and indexed values.
+-- @tparam int ordering Current ordering index.
 local function ordered(sorted, ordering)
   local index = sorted[1][ordering + 1]
   return index and ordering + 1, index, sorted[2][index]
@@ -13,6 +16,8 @@ end
 --- Iterates pairs by sorted indices.
 -- Iterates a table by first sorting its indices.
 -- First collects the indices.
+-- Then sorts them using the optional comparison function.
+-- Finally, iterates the sorted indices and their corresponding values.
 -- @tparam tab indexed Index-value pairs.
 -- @tparam ?func comparing Index comparison function.
 function _M.ipairs(indexed, comparing)
