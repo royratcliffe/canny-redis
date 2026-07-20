90f413326031c35af9c89c5c5a84b7cba3d0578d
Roy Ratcliffe <roy@ratcliffe.me>
2026-07-20 13:04:43 +0100

    fix(interleaved): improve value retrieval in inext function
    Refactor the inext function to retrieve both first and second
    values from the indexed array in a single line, enhancing
    readability and maintainability.

--- interleaved.lua
+++ interleaved.lua
@@ -12,8 +12,8 @@ local _M = {}
 -- @return First interleaved value.
 -- @return Second interleaved value.
 function _M.inext(indexed, index)
-  local first = indexed[index + 1]
-  return first and index + 2, first, indexed[index + 2]
+  local first, second = indexed[index + 1], indexed[index + 2]
+  return (first ~= nil or second ~= nil) and index + 2 or nil, first, second
 end
 
 --- Iterates an interleaved array of pairs.
